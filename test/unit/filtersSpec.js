c06ff406f624120e56380ec9cbe57417b80182a6
Luqmaan <ldawoodjee@gmail.com>
2014-04-18 21:57:39 -0500

    Simplify filters test injection

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -1,18 +1,16 @@
 describe('filter', function() {
-    var directionifyFilter;
     // https://github.com/frapontillo/angular-filters/blob/master/test/default/defaultSpec.js
 
     beforeEach(module('metroRappid.filters'));
-    beforeEach(inject(function($filter) {
-        directionifyFilter = $filter('directionify');
-    }));
 
     describe('directionify', function() {
-        it('should turn a direction number into a string', function() {
-            expect(directionifyFilter(0)).toEqual('North');
-            expect(directionifyFilter(1)).toEqual('South');
-            expect(directionifyFilter(2)).toEqual('East');
-            expect(directionifyFilter(3)).toEqual('West');
-        });
+        it('should turn a direction number into a string', inject(function($filter) {
+            var directionify = $filter('directionify');
+
+            expect(directionify(0)).toEqual('North');
+            expect(directionify(1)).toEqual('South');
+            expect(directionify(2)).toEqual('East');
+            expect(directionify(3)).toEqual('West');
+        }));
     });
 });
